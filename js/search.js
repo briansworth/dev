2fa68c730d08c83d6de09e40e039a1933cbc30ea
briansworth <b.farnsworth47@gmail.com>
2019-05-12 10:53:55 -0400

    Search result no longer using list item

--- js/search.js
+++ js/search.js
@@ -43,7 +43,7 @@ jQuery(function() {
           var item = loaded_data[result.ref];
 
           // Build a snippet of HTML for this result
-          var appendString = '<li><a href="' + item.url + '">' + item.title + '</a></li>';
+          var appendString = '<a href="' + item.url + '">' + item.title + '</a>';
 
           // Add the snippet to the collection of results.
           $search_results.append(appendString);
