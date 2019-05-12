8fb844a0ff9ac56f21652cd7ea8430f70a7fa80c
briansworth <b.farnsworth47@gmail.com>
2019-05-12 10:55:54 -0400

    Added line break to end of earch result

--- js/search.js
+++ js/search.js
@@ -43,14 +43,14 @@ jQuery(function() {
           var item = loaded_data[result.ref];
 
           // Build a snippet of HTML for this result
-          var appendString = '<a href="' + item.url + '">' + item.title + '</a>';
+          var appendString = '<a href="' + item.url + '">' + item.title + '</a><br>';
 
           // Add the snippet to the collection of results.
           $search_results.append(appendString);
         });
       } else {
         // If there are no results, let the user know.
-        $search_results.html('<li>Nothing found...</li>');
+        $search_results.html('Nothing found...');
       }
     });
   }
